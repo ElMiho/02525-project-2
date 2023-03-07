f94025a36ccbcdffbc0b25777ea52dec1e417332
ElMiho <mikael.hjermitslev@gmail.com>
2023-03-07 10:39:30 +0100

    Update single and all

--- code/all_spectral_bands.py
+++ code/all_spectral_bands.py
@@ -4,39 +4,93 @@ import scipy.io as io
 import matplotlib.pyplot as plt
 from sklearn import metrics
 
+# Same percentage of meat and fat
+p1 = 0.5
+p2 = 1 - p1
+
 folder = ".././files-from-learn/data/"
 
 multispectral_day01 = io.loadmat(folder + "multispectral_day01.mat")["immulti"]
 annotation_day01 = PIL.Image.open(folder + "annotation_day01.png")
 annotation_day01 = np.asarray(annotation_day01)
 
-red_channel_mask = (annotation_day01[:,:,0] > 0)
-green_channel_mask = (annotation_day01[:,:,1] > 0)
-blue_channel_mask = (annotation_day01[:,:,2] > 0)
+unknown_channel_mask = (annotation_day01[:,:,0] > 0)
+fat_channel_mask = (annotation_day01[:,:,1] > 0)
+meat_channel_mask = (annotation_day01[:,:,2] > 0)
 
 mean_meat_list = []
 mean_fat_list = []
 
 salami_mask = np.logical_or(
-    np.logical_or(red_channel_mask, green_channel_mask), 
-    blue_channel_mask)
+    np.logical_or(unknown_channel_mask, fat_channel_mask), 
+    meat_channel_mask)
 
+# print(np.average(multispectral_day01, 2, np.repeat(np.expand_dims(meat_channel_mask.astype(np.int32), axis=2), 19, 2)).size())
+
+
+def S(x, mean, cov_matrix_inv, p):
+    res = np.dot(x, cov_matrix_inv @ mean) - 1/2 * np.dot(mean, cov_matrix_inv @ mean) + np.log(p)
+    return res
+
+data_vecs = []
+
+fat_mask_actual = np.logical_and(fat_channel_mask, salami_mask)
+meat_mask_actual = np.logical_and(meat_channel_mask, salami_mask)
 for i in range(19):
     channel = multispectral_day01[:,:,i]
-    masked_channel_meat = channel[red_channel_mask]
-    # Assuming that both green and blue is fat 
-    fat_mask_actual = np.logical_and(
-        np.logical_or(green_channel_mask, blue_channel_mask), 
-        salami_mask)
+    masked_channel_meat = channel[meat_mask_actual]
+
+    salami_channel = channel[salami_mask]
+    data_vecs.append(salami_channel.flatten())
+
+    # Assuming that both green and blue is fat
     masked_channel_fat = channel[fat_mask_actual]
 
-    
     mean_meat = masked_channel_meat.mean()
     mean_fat = masked_channel_fat.mean()
 
     mean_fat_list.append(mean_fat)
     mean_meat_list.append(mean_meat)
 
-# Calculate S1 (fat) and S2 (meat)
-p1 = 0.5
-p2 = 1 - p1
+mean_fat_list = np.asarray(mean_fat_list)
+mean_meat_list = np.asarray(mean_meat_list)
+
+data_vecs = np.asarray(data_vecs)
+cov_matrix = np.cov(data_vecs)
+cov_inverse = np.linalg.inv(cov_matrix)
+print(f"Covariance matrix, shape: {cov_matrix.shape}\n{cov_matrix}")
+
+fat_mask_predicted = np.zeros((514,514))
+im_meat_predicted = np.zeros((514,514))
+
+data = multispectral_day01
+
+for i in range(514):
+    for j in range(514):
+        if not salami_mask[i,j]: continue
+        x = data[i,j,:]
+        # Calculate S1 and S2
+        s1 = S(x,mean_fat_list, cov_inverse, p1)
+        s2 = S(x,mean_meat_list, cov_inverse, p2)
+        if s1 >= s2:
+            fat_mask_predicted[i,j] = 1
+        else:
+            im_meat_predicted[i,j] = 1
+
+confusion_matrix = metrics.confusion_matrix(fat_mask_actual.astype(np.int32).flatten(), fat_mask_predicted.astype(np.int32).flatten())
+error = 1 - (confusion_matrix[0,0] + confusion_matrix[1,1])/confusion_matrix.sum()
+print(f"Error: {error}")
+
+plt.subplot(1,3,1)
+plt.title("Fat predicted")
+plt.imshow(fat_mask_predicted)
+
+plt.subplot(1,3,2)
+plt.title("Annotation")
+plt.imshow(annotation_day01)
+
+plt.subplot(1,3,3)
+plt.title("Meat predicted")
+plt.imshow(im_meat_predicted)
+
+plt.show()
--- code/single_spectral_band.py
+++ code/single_spectral_band.py
@@ -10,42 +10,41 @@ multispectral_day01 = io.loadmat(folder + "multispectral_day01.mat")["immulti"]
 annotation_day01 = PIL.Image.open(folder + "annotation_day01.png")
 annotation_day01 = np.asarray(annotation_day01)
 
-red_channel_mask = (annotation_day01[:,:,0] > 0)
-green_channel_mask = (annotation_day01[:,:,1] > 0)
-blue_channel_mask = (annotation_day01[:,:,2] > 0)
+# RED: UNKNOWN, BLUE: MEAT, GREEN: FAT
+unknown_channel_mask = (annotation_day01[:,:,0] > 0)
+fat_channel_mask = (annotation_day01[:,:,1] > 0)
+meat_channel_mask = (annotation_day01[:,:,2] > 0)
 
 salami_mask = np.logical_or(
-    np.logical_or(red_channel_mask, green_channel_mask), 
-    blue_channel_mask)
+    np.logical_or(unknown_channel_mask, fat_channel_mask), 
+    meat_channel_mask)
 
 thresholds = []
 confusion_matrices = []
 error_rates = []
 fat_masks = []
+meat_masks = []
 
 for i in range(19):
     channel = multispectral_day01[:,:,i]
-    masked_channel_meat = channel[red_channel_mask]
-    # Assuming that both green and blue is fat 
-    fat_mask_actual = np.logical_and(
-        np.logical_or(green_channel_mask, blue_channel_mask), 
-        salami_mask)
+    meat_mask_actual = np.logical_and(meat_channel_mask, salami_mask)
+    masked_channel_meat = channel[meat_mask_actual]
+    
+    
+    fat_mask_actual = np.logical_and(fat_channel_mask, salami_mask)
     masked_channel_fat = channel[fat_mask_actual]
 
-    
     mean_meat = masked_channel_meat.mean()
     mean_fat = masked_channel_fat.mean()
     
-    variance_meat = masked_channel_meat.var()
-    variance_fat = masked_channel_fat.var()
-    
-    # Threshold values
+    # Threshold values (gotten by solving normal_dist(mu_mean) = normal_dist(mu_fat))
     t = 1/2 * (mean_meat + mean_fat)
     thresholds.append(t)
 
     # If t >= mu_fat, then all values < t is classified as being fat, and all others meat
     if t >= mean_fat:
         fat_mask_predicted = np.logical_and(channel < t, salami_mask)
+        meat_mask_predicted = np.logical_and(channel >= t, salami_mask)
         # meat_threshold_mask = np.logical_and(channel >= t, salami_mask)
 
         # Confusion matrix
@@ -59,6 +58,7 @@ for i in range(19):
     # Otherwise all values >= t is classified as being fat
     else:
         fat_mask_predicted = np.logical_and(channel >= t, salami_mask)
+        meat_mask_predicted = np.logical_and(channel < t, salami_mask)
         # meat_threshold_mask = np.logical_and(channel < t, salami_mask)
         
         # Confusion matrix
@@ -70,8 +70,9 @@ for i in range(19):
         error_rates.append(error)
 
     fat_masks.append(fat_mask_predicted)
+    meat_masks.append(meat_mask_predicted)
 
-    print(f"[{i+1}/19] --- mean_meat = {mean_meat} --- variance_meat = {variance_meat}\n\t--- mean_fat = {mean_fat} --- variance_fat = {variance_fat}")
+    print(f"[{i+1}/19] --- mean_meat = {mean_meat} --- mean_fat = {mean_fat}")
 
 print(thresholds)
 
@@ -90,8 +91,28 @@ best_spectral_band = np.argmin(error_rates)
 
 print(f"Best spectral band: {1+best_spectral_band}")
 
+
+
+plt.tight_layout()
+plt.subplot(1,3,1)
+plt.title("Fat mask")
 fat_mask = fat_masks[best_spectral_band]
 plt.imshow(fat_mask)
+
+plt.subplot(1,3,2)
+plt.title("Annotations")
+plt.imshow(annotation_day01)
+
+plt.subplot(1,3,3)
+plt.title("Meat mask")
+meat_mask = meat_masks[best_spectral_band]
+plt.imshow(meat_mask)
+plt.show()
+
+plt.title("Meat prediction")
+test = annotation_day01.copy()
+test[:,:,0] = meat_mask * 255
+plt.imshow(test)
 plt.show()
 
 # plt.matshow(annotation_day01)
